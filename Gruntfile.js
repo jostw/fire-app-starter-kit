23a54bada8e9576805264dfaf62e7bc75edc15a5
jos <mail@jos.tw>
2014-04-08 02:56:36 +0000

    Add: load-grunt-tasks and watch to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json")
+        pkg: grunt.file.readJSON("package.json"),
+
+	watch: {
+            options: {
+                livereload: true
+            }
+        }
     });
+
+    // Load the plugins.
+    require("load-grunt-tasks")(grunt);
+ 
+    // Default task(s).
+    grunt.registerTask("default", ["watch"]);
 };
 
