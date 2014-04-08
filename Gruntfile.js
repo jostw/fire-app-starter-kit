750314a2a067738dc05c1cb6eb9e4b8f4615df93
jos <mail@jos.tw>
2014-04-08 08:39:41 +0000

    Add: jshint to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,40 @@
+/*
+ * hello-project
+ *
+ * https://github.com/jostw/hello-project
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2014 jos
+ */
+
+"use strict";
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
 
-	watch: {
+        watch: {
             options: {
                 livereload: true
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+
+            grunt: {
+                files: {
+                    src: ["Gruntfile.js"]
+                }
             }
         }
     });
 
     // Load the plugins.
     require("load-grunt-tasks")(grunt);
- 
-    // Default task(s).
-    grunt.registerTask("default", ["watch"]);
-};
 
+    // Default task(s).
+    grunt.registerTask("default", ["jshint:grunt", "watch"]);
+};
