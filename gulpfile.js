8aaa4aa1d85883c59d5e9a59308ef4dafcbeb47b
jos <mail@jos.tw>
2014-04-30 07:33:51 +0000

    Add: Gulp initial files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/*
+ * hello-project
+ *
+ * https://github.com/jostw/hello-project
+ *
+ * Copyright (c) 2014 jos
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+var gulp = require("gulp");
+
+gulp.task("default", function() {
+
+});
